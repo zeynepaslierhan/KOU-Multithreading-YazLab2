0e201b9ae65c58a7fbb934cabfae682bca4de24c
Zeynep Asli Erhan <zeynepaslierhan@gmail.com>
2022-11-21 21:46:55 +0300

    csv dosyasının bölünmesi

--- src/Divide_csv.py
+++ src/Divide_csv.py
@@ -1,8 +1,11 @@
 
 
-def divide_csvFile(lines_divided_number,filename):
+def divide_csvFile(part):
+    filename=0
     csvfile = open("C:/Users/zerha/Documents/GitHub/KOU-Multithreading-YazLab2/src/data_set/littleData.csv", 'r').readlines()
-    for i in range(len(csvfile)):
+    length=len(csvfile)
+    lines_divided_number=int(length/part)
+    for i in range(length):
         if i % lines_divided_number == 0:
             if i == 0: #Sütun bilgilerinin bölünmüş dosyalarda yazılmaması için
                 open('C:/Users/zerha/Documents/GitHub/KOU-Multithreading-YazLab2/src/divided_CsvFile/'+ str(filename) + '.csv', 'w+').writelines(csvfile[i+1:i+lines_divided_number])
