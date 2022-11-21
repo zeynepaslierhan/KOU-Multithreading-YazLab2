3d463230e928f0d6505384d2c791b1858c33d4fc
Zeynep Asli Erhan <zeynepaslierhan@gmail.com>
2022-11-21 23:29:50 +0300

    Update Divide_csv.py

--- src/Divide_csv.py
+++ src/Divide_csv.py
@@ -6,8 +6,10 @@ def divide_csvFile(part):
     for i in range(length):
         if i % lines_divided_number == 0:
             if i == 0: #Sütun bilgilerinin bölünmüş dosyalarda yazılmaması için
-                open('C:/Users/hkf_4/Documents/GitHub/KOU-Multithreading-YazLab2/src/divided_CsvFile/'+ str(filename) + '.csv', 'w+').writelines(csvfile[i+1:i+lines_divided_number])
+                file_newName='C:/Users/hkf_4/Documents/GitHub/KOU-Multithreading-YazLab2/src/divided_CsvFile/'+str(filename)+'.csv'
+                open(file_newName, 'w+').writelines(csvfile[i+1:i+lines_divided_number])
             else:
-                open('C:/Users/hkf_4/Documents/GitHub/KOU-Multithreading-YazLab2/src/divided_CsvFile/'+ str(filename) + '.csv', 'w+').writelines(csvfile[i:i+lines_divided_number])
+                file_newName='C:/Users/hkf_4/Documents/GitHub/KOU-Multithreading-YazLab2/src/divided_CsvFile/'+str(filename)+'.csv'
+                open(file_newName, 'w+').writelines(csvfile[i:i+lines_divided_number])
             filename += 1
     return filename
